7f3b9bd31759cbd5d66fb2ba63631ba99d2789ae
Piotr Rapacz <piotr.rapacz@britnet.com.pl>
2023-03-16 19:11:36 +0100

    TODO

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,13 @@
+// about formatter usage - https://unicode-org.github.io/icu/userguide/format_parse/messages/
+// plural rules - https://cldr.unicode.org/index/cldr-spec/plural-rules
+// plural rules chart for languages - https://unicode-org.github.io/cldr-staging/charts/37/supplemental/language_plural_rules.html
+// online editor - https://format-message.github.io/icu-message-format-for-translators/editor.html
+
+//add dependency "com.ibm.icu:icu4j:72.1"
+//create ResourceBundle
+//getBundle
+//MessageFormat
+
 
 fun main(){
     println("Hello World")
